130e008ee16832ed633ab0312d3120bc98ebacd6
Medha <medha8pandey@gmail.com>
2022-05-26 23:07:14 +0530

    8| Delete a node in LinkedList

---
+++ leetcode/editor/en/237.delete-node-in-a-linked-list.kt
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=237 lang=kotlin
+ *
+ * [237] Delete Node in a Linked List
+ */
+
+// @lc code=start
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun deleteNode(node: ListNode?) {
+        if(node == null) return
+        node.`val` = node.next.`val`
+        node.next = node.next?.next 
+    }
+}
+// @lc code=end
+
