931e89afc597dfb8381bfc0881668dd16d42a295
Medha Pandey <medha8pandey@gmail.com>
2022-05-31 01:53:04 +0530

    17 | Maximum Sub Array Sum

---
+++ leetcode/editor/en/53.maximum-subarray.kt
@@ -0,0 +1,21 @@
+import java.lang.Integer.max
+//O(N) Time and O(1) Space
+//Kadane's Algorithm
+class Solution {
+    fun maxSubArray(nums: IntArray): Int {
+
+        var localSum = 0
+        var globalSum = nums[0]
+
+        nums.forEach{
+            localSum += it
+            if(localSum >globalSum){
+                globalSum = max(globalSum,localSum)
+            }
+            if(localSum < 0){
+                localSum = 0
+            }
+        }
+        return globalSum
+    }
+}
