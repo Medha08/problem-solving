f0b3d36dc00b9139366bbbbca9aea9c6e9e25a48
Medha Pandey <medha8pandey@gmail.com>
2022-06-13 23:43:32 +0530

    38| LinkedList Cycle

---
+++ leetcode/editor/en/141.linked-list-cycle.kt
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=141 lang=kotlin
+ *
+ * [141] Linked List Cycle
+ */
+
+// @lc code=start
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class LinkedListCycle {
+    fun hasCycle(head: ListNode?): Boolean {
+        if(head == null) return false
+        var slow = head
+        var fast = head!!.next
+        while (slow != fast) {
+            slow = slow?.next
+            fast = fast?.next?.next
+            if(slow == null) return false 
+        }
+        return true
+    }
+}
+// @lc code=end
+
